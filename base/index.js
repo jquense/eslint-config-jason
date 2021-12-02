c59aef1aa56e64b48f1522a0104fc6fefd6ee290
Jason Quense <monastic.panic@gmail.com>
2021-12-02 08:56:29 -0500

    feat: bump stuff

--- base/index.js
+++ base/index.js
@@ -1,8 +1,8 @@
-const confusingBrowserGlobals = require('confusing-browser-globals')
+const confusingBrowserGlobals = require("confusing-browser-globals");
 
 module.exports = {
-  parser: require.resolve('babel-eslint'),
-  extends: ['eslint:recommended', 'plugin:import/errors'],
+  parser: require.resolve("@babel/eslint-parser"),
+  extends: ["eslint:recommended", "plugin:import/errors"],
   env: {
     es6: true,
     node: true,
@@ -10,7 +10,7 @@ module.exports = {
   },
 
   parserOptions: {
-    sourceType: 'module',
+    sourceType: "module",
     ecmaVersion: 2018,
     ecmaFeatures: {
       jsx: true,
@@ -20,131 +20,132 @@ module.exports = {
   globals: {
     __DEV__: false,
   },
-  plugins: ['import'],
+  plugins: ["import"],
   settings: {
-    'import/extensions': ['.js', '.ts', '.tsx'],
-    'import/parsers': {
-      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx'],
+    "import/extensions": [".js", ".ts", ".tsx"],
+    "import/parsers": {
+      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx"],
       // needed for typescript files importing js, since the configured parser
-      // will be ts not babel-eslint for the host file
-      [require.resolve('babel-eslint')]: ['.js'],
+      // will be ts not @babel/eslint-parser for the host file
+      [require.resolve("@babel/eslint-parser")]: [".js"],
     },
-    'import/resolver': {
+    "import/resolver": {
       node: {
-        extensions: ['.js', '.ts', '.tsx'],
+        extensions: [".js", ".ts", ".tsx"],
       },
     },
   },
   rules: {
-    'prefer-const': 'off',
-    'global-require': 'error',
-    'comma-spacing': 'error',
-    'constructor-super': 'error',
-    'func-call-spacing': 'error',
-    'dot-notation': ['error', { allowKeywords: true }],
-    'eol-last': 'error',
+    "prefer-const": "off",
+    "global-require": "error",
+    "comma-spacing": "error",
+    "constructor-super": "error",
+    "func-call-spacing": "error",
+    "dot-notation": ["error", { allowKeywords: true }],
+    "eol-last": "error",
     // disallow specific globals
-    'no-restricted-globals': ['error', ...confusingBrowserGlobals],
+    "no-restricted-globals": ["error", ...confusingBrowserGlobals],
     quotes: [
-      'error',
-      'single',
+      "error",
+      "single",
       {
         avoidEscape: true,
         allowTemplateLiterals: true,
       },
     ],
 
-    'no-cond-assign': 'off',
-    'no-this-before-super': 'error',
-    'no-trailing-spaces': 'error',
-    'no-unused-expressions': [
+    "no-cond-assign": "off",
+    "no-this-before-super": "error",
+    "no-trailing-spaces": "error",
+    "no-unused-expressions": [
       2,
       {
         allowTernary: true,
         allowShortCircuit: true,
       },
     ],
-    'no-unused-vars': [
-      'error',
+    "no-unused-vars": [
+      "error",
       {
-        vars: 'all',
-        varsIgnorePattern: '^_',
-        args: 'after-used',
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
         ignoreRestSiblings: false,
-        argsIgnorePattern: '^_',
+        argsIgnorePattern: "^_",
       },
     ],
 
-    'import/no-duplicates': 'error',
+    "import/no-duplicates": "error",
   },
 
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
-      extends: 'plugin:@typescript-eslint/recommended',
+      files: ["*.ts", "*.tsx"],
+      extends: "plugin:@typescript-eslint/recommended",
       rules: {
-        'prefer-const': 'off',
-        'import/named': 'off',
-        'import/no-unresolved': 'off',
-        'import/default': 'off',
-        'import/namespace': 'off',
-        'no-use-before-define': 'off',
-        'no-useless-constructor': 'off',
-        'no-unused-expressions': 'off',
+        "prefer-const": "off",
+        "import/named": "off",
+        "import/no-unresolved": "off",
+        "import/default": "off",
+        "import/namespace": "off",
+        "no-use-before-define": "off",
+        "no-useless-constructor": "off",
+        "no-inner-declarations": "off",
+        "no-unused-expressions": "off",
 
-        'import/export': 'off',
-        'import/no-duplicates': 'off',
-        '@typescript-eslint/no-this-alias': 'off',
-        '@typescript-eslint/ban-types': 'off',
-        '@typescript-eslint/ban-ts-comment': 'off',
-        '@typescript-eslint/consistent-type-assertions': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/naming-convention': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-non-null-assertion': 'off',
-        '@typescript-eslint/no-unused-expressions': [
-          'error',
+        "import/export": "off",
+        "import/no-duplicates": "off",
+        "@typescript-eslint/no-this-alias": "off",
+        "@typescript-eslint/ban-types": "off",
+        "@typescript-eslint/ban-ts-comment": "off",
+        "@typescript-eslint/consistent-type-assertions": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/naming-convention": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-unused-expressions": [
+          "error",
           {
             allowTernary: true,
             allowShortCircuit: true,
           },
         ],
-        '@typescript-eslint/no-shadow': [
-          'error',
+        "@typescript-eslint/no-shadow": [
+          "error",
           {
             ignoreTypeValueShadow: true,
             ignoreFunctionTypeParameterNameValueShadow: true,
           },
         ],
-        '@typescript-eslint/no-unused-vars': [
-          'error',
+        "@typescript-eslint/no-unused-vars": [
+          "error",
           {
-            vars: 'all',
-            varsIgnorePattern: '^_',
-            args: 'after-used',
+            vars: "all",
+            varsIgnorePattern: "^_",
+            args: "after-used",
             ignoreRestSiblings: false,
-            argsIgnorePattern: '^_',
+            argsIgnorePattern: "^_",
           },
         ],
       },
     },
     {
       files: [
-        'tools/**',
-        'karma.conf.js',
-        'webpack.config.js',
-        'test/**',
-        '**/__tests__/**',
-        '**/__mocks__/**',
+        "tools/**",
+        "karma.conf.js",
+        "webpack.config.js",
+        "test/**",
+        "**/__tests__/**",
+        "**/__mocks__/**",
       ],
       rules: {
-        'global-require': 'off',
-        'no-dynamic-require': 'off',
-        'import/no-extraneous-dependencies': [
-          'error',
+        "global-require": "off",
+        "no-dynamic-require": "off",
+        "import/no-extraneous-dependencies": [
+          "error",
           { devDependencies: true },
         ],
       },
     },
   ],
-}
+};
